82636be3fedf0cd3eed7db39eab501875b03d43a
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2022-12-29 14:47:34 +0100

    add settings file

---
+++ src/storks/storkssettings.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef STORKSETT
+#define STORKSETT
+#include "common.hpp"
+
+/**
+ * @brief StorksSettings
+ * @author Mads Kjeldgaard
+ * @date 08-10-2022
+ * @details
+ */
+namespace storkspace {
+
+class Settings {
+
+  // std::string scale{"Major"};
+  // std::map<std::string, int> mapname{{"GPU", 5}, {"CPU", 15}};
+
+};
+
+} // namespace storkspace
+#endif
